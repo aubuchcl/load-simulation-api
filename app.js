d844189d89b40d8c1c26d554f11bf70550b42eae
Chris Aubuchon <aubuchcl@gmail.com>
2025-06-03 08:25:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { simulateLoad } = require('./load');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '3mb' }));
 
 // Utility to enforce safe input bounds
 function safe(value, min, max) {
